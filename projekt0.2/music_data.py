7bef934e2008a1a3d9e7e9ce43b3b62211cfc34a
zaranowska <56322442+zaranowska@users.noreply.github.com>
2023-01-15 11:44:05 +0100

    Update music_data.py

--- projekt0.2/music_data.py
+++ projekt0.2/music_data.py
@@ -6,9 +6,9 @@ from data_functions import offset_api_limit, get_artists_df, get_tracks_df, get_
 
 
 
-client_id = "a811ca65f86749e385a0426469546013"
-client_secret = "499683208dac4b1487cfbc47bf90cbc3"
-redirect_uri = "http://localhost:8888/callback"
+client_id = "CLIENT_ID"
+client_secret = "CLIENT_SECRET"
+redirect_uri = "REDIRECT_URI"
 
 
 scope = "user-library-read user-follow-read user-top-read playlist-read-private"
@@ -63,8 +63,7 @@ def playlist_yaml_dump(playlist_tracks_df):
 def recommendation_tracks(sp, playlist_tracks_df):
  
     recommendation_tracks = get_recommendations(sp, playlist_tracks_df[playlist_tracks_df['playlist_name'].isin(
-     ["Your Top Songs 2022", 'Chill', 'Top Metal', 'Top Pop', 'Dark&Gothic']
-                 )].drop_duplicates(subset='id', keep="first")['id'].tolist())
+        "Your Top Songs 2022")].drop_duplicates(subset='id', keep="first")['id'].tolist())
     recommendation_tracks_df = get_tracks_df(recommendation_tracks)
     recommendation_tracks_df = get_track_audio_df(sp, recommendation_tracks_df)
     recommendation_tracks_df.to_pickle("recommendation_tracks.pkl")
